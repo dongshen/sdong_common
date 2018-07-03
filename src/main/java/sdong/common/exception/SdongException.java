edacfec134c54aad18be6593ca28e904a5e07d87
shendong <shendong70@163.com>
2018-07-03 10:41:50 +0800

    add exception in common

---
+++ src/main/java/sdong/common/exception/SdongException.java
@@ -0,0 +1,32 @@
+package sdong.common.exception;
+
+public class SdongException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 980983433918588495L;
+	
+	private String reason;
+
+	public SdongException(Exception cause) {
+		super(cause);
+		this.reason = cause.getMessage();
+
+	}
+
+	public SdongException(Throwable cause) {
+		super(cause);
+		this.reason = cause.getMessage();
+	}
+
+	public SdongException(String message) {
+		super(message);
+		this.reason = message;
+	}
+
+	public String getMessage() {
+		return reason;
+	}
+
+}
