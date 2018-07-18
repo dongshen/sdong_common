7201ddc9a15fd9ccf76735bc2ae87b76fd15c1eb
shendong <shendong70@163.com>
2018-07-18 14:11:41 +0800

    add splitStringToListByLineBreak

--- src/main/java/sdong/common/utils/StringUtil.java
+++ src/main/java/sdong/common/utils/StringUtil.java
@@ -1,6 +1,16 @@
 package sdong.common.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StringUtil {
+	private static final Logger logger = LoggerFactory.getLogger(StringUtil.class);
 
 	public static final int checkIndentNum(String line, String checkChar) {
 		int indented = 0;
@@ -10,5 +20,30 @@ public class StringUtil {
 		}
 
 		return indented;
+	}
+
+	public static final List<String> splitStringToListByLineBreak(String str) {
+		BufferedReader reader = null;
+		List<String> list = new ArrayList<String>();
+		try {
+			reader = new BufferedReader(new StringReader(str));
+			for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+				list.add(line);
+			}
+			reader.close();
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		return list;
 	}
 }
--- src/test/java/sdong/common/utils/StringUtilTest.java
+++ src/test/java/sdong/common/utils/StringUtilTest.java
@@ -1,6 +1,9 @@
 package sdong.common.utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,20 +14,39 @@ public class StringUtilTest {
 		String line = "tree = TF_func:";
 		String checkChar = " ";
 		int intended = StringUtil.checkIndentNum(line, checkChar);
-		assertEquals(0,intended);
-		
-		line= "  f = Function:";
+		assertEquals(0, intended);
+
+		line = "  f = Function:";
 		intended = StringUtil.checkIndentNum(line, checkChar);
-		assertEquals(2,intended);
-		
-		line= "        return = scalar_type_t:";
+		assertEquals(2, intended);
+
+		line = "        return = scalar_type_t:";
 		intended = StringUtil.checkIndentNum(line, checkChar);
-		assertEquals(8,intended);
-		
-		line= "          kind = void";
+		assertEquals(8, intended);
+
+		line = "          kind = void";
 		intended = StringUtil.checkIndentNum(line, checkChar);
-		assertEquals(10,intended);
-				
+		assertEquals(10, intended);
+
 	}
 
+	@Test
+	public void testSplitStringToListByLineBreak() {
+		List<String> list = new ArrayList<String>();
+		list.add("");
+		list.add("");
+		list.add("line1");
+		list.add("");
+		list.add("line2");
+		list.add("");
+		list.add("");
+		list.add("line3");
+		list.add("");
+		list.add("");
+
+		String str = String.join("\r\n", list) + "\r\n";
+		List<String> result = StringUtil.splitStringToListByLineBreak(str);
+		assertEquals(list.size(), result.size());
+
+	}
 }
