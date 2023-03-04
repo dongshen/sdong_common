9918d17f0c0c65ede5e4e044b6ebd4494e2c1bcb
shendong <shendong70@163.com>
2023-03-04 22:43:10 +0800

    exception add fail position

--- src/main/java/sdong/common/exception/SdongException.java
+++ src/main/java/sdong/common/exception/SdongException.java
@@ -2,35 +2,53 @@ package sdong.common.exception;
 
 public class SdongException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 980983433918588495L;
-	
-	private String reason;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 980983433918588495L;
 
-	public SdongException(Exception cause) {
-		super(cause);
-		this.reason = cause.getMessage();
+    private String reason;
 
-	}
+    public SdongException(Exception cause) {
+        super(cause);
+        this.reason = cause.getMessage();
 
-	public SdongException(Throwable cause) {
-		super(cause);
-		this.reason = cause.getMessage();
-	}
+    }
 
-	public SdongException(String message) {
-		super(message);
-		this.reason = message;
-	}
+    public SdongException(Throwable cause) {
+        super(cause);
+        this.reason = cause.getMessage();
+    }
+
+    public SdongException(String message) {
+        super(message);
+        this.reason = message;
+    }
 
     public SdongException(String message, Throwable cause) {
         super(message, cause);
-		this.reason = message;
+        this.reason = message;
     }
 
-	public String getMessage() {
-		return reason;
-	}
+    @Override
+    public String getMessage() {
+        return reason;
+    }
+
+    /**
+     * Get exception throwable position info
+     * 
+     * @return class.method:lineNumber
+     */
+    public String getErrorPosition() {
+        StackTraceElement[] stElements = getStackTrace();
+        if (stElements == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        StackTraceElement st = stElements[0];
+        sb.append(st.getClassName()).append(".").append(st.getMethodName()).append(":")
+                .append(st.getLineNumber());
+        return sb.toString();
+    }
 }
